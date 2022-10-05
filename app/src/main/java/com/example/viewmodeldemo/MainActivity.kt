79c98408a9173dee7b81878bca18db04c4c64cfd
Valdo Mpinga <valdompinga57@gmail.com>
2022-10-05 11:43:24 +0100

    App done

---
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.viewmodeldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var viewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        val textView = findViewById<TextView>(R.id.tvCounter)
+        val button = findViewById<TextView>(R.id.btnIncrement)
+
+     //   textView.text = count.toString()
+     //   textView.text = viewModel.count.toString()
+
+        viewModel.count.observe(this, Observer{
+            textView.text = it.toString()
+        })
+
+        button.setOnClickListener {
+          //  ++count
+          //  textView.text = count.toString()
+
+            viewModel.updateCount()
+            //textView.text = viewModel.count.toString()
+        }
+    }
+}
